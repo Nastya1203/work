62243296d7d40c6e76c2e58541e546f8c8bc1c0b
Anastasia <miki_nastya@mail.ru>
2023-09-29 12:07:38 +0800

    добавили изменения

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -8,7 +8,7 @@ int[] RandomArray(int arrLenght)
 {
     int[] arr = new int[arrLenght];
     Random rand = new Random();
-    for (int i = 0; i < arrLenght; i ++)
+    for (int i = 0; i < arrLenght; i++)
     {
         arr[i] = rand.Next(2);
     }
@@ -30,16 +30,30 @@ void PrintArray2(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-       if (i < array.Length - 1)
-       {
-        Console.Write($"{array[i]}, ");
-       }
-       else
-       {
-        Console.Write($"{array[i]}]");
-       }
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}]");
+        }
     }
 }
 
 int[] arr = RandomArray(8);
 PrintArray2(arr);
+
+
+
+// int[] array = new int[8];
+// int[] array1 = new int[8] { 5, 6, 9, 8, 9, 9, 0, 6 };
+// int[] array2 = new int[] { 5, 6, 9, 8, 9, 9, 0, 6 };
+// int[] array3 = { 5, 6, 9, 8, 9, 9, 0, 6 };
+
+// var array4 = new int[8];
+
+// bool[] arrayB = new bool[8];
+// double[] arrayD = new double[8];
+
+// Random[] rnd = new Random[8];
